39f11fbe331a888384549457114cfce611116ec4
zeeshan <zeesa18.2@gmail.com>
2023-04-14 12:33:45 +0500

    added port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/.well-known/assetlinks.json', (req, res) => {
   const filePath = path.join(__dirname, 'assetLinks.json');
